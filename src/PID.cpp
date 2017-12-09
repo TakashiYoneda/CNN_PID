a68c8f26c7864664f407240cb4f21bde11c4886f
TakashiYoneda <TakashiYoneda@users.noreply.github.com>
2017-12-09 09:13:32 +0900

    Add files via upload

---
+++ src/PID.cpp
@@ -0,0 +1,30 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+  Kp_ = Kp;
+  Ki_ = Ki;
+  Kd_ = Kd;
+  p_error = 0.0f;
+  i_error = 0.0f;
+  d_error = 0.0f;
+}
+
+void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  i_error += cte;
+  p_error = cte;
+}
+
+double PID::TotalError() {
+  return Kp_*p_error + Ki_*i_error + Kd_*d_error;
+}
